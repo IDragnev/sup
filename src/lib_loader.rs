7d4e081ddd9eb343e11a284c2674c5773ef2111b
Iliyan <iddragnev@gmail.com>
2022-03-27 15:39:38 +0300

    Rename Err enums

--- src/lib_loader.rs
+++ src/lib_loader.rs
@@ -24,32 +24,32 @@ pub struct Library {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum LoadLibErr {
+pub enum LoadLibError {
     LibNotFound,
     InvalidLibName,
 }
 
 #[derive(Debug, PartialEq)]
-pub enum GetProcErr {
+pub enum GetProcError {
     ProcNotFound,
     InvalidProcName,
 }
 
 impl Library {
-    pub fn new(name: &str) -> Result<Self, LoadLibErr> {
-        let c_name = CString::new(name).or(Err(LoadLibErr::InvalidLibName))?;
+    pub fn new(name: &str) -> Result<Self, LoadLibError> {
+        let c_name = CString::new(name).or(Err(LoadLibError::InvalidLibName))?;
         let l = unsafe { LoadLibraryA(c_name.as_ptr()) };
 
         l.map(|module| Library { module })
-         .ok_or(LoadLibErr::LibNotFound)
+         .ok_or(LoadLibError::LibNotFound)
     }
 
-    pub fn get_proc<T>(&self, name: &str) -> Result<T, GetProcErr> {
-        let c_name = CString::new(name).or(Err(GetProcErr::InvalidProcName))?;
+    pub fn get_proc<T>(&self, name: &str) -> Result<T, GetProcError> {
+        let c_name = CString::new(name).or(Err(GetProcError::InvalidProcName))?;
         let proc = unsafe { GetProcAddress(self.module, c_name.as_ptr()) };
 
         proc.map(|p| unsafe { transmute_copy(&p) })
-            .ok_or(GetProcErr::ProcNotFound)
+            .ok_or(GetProcError::ProcNotFound)
     }
 }
 
@@ -62,13 +62,13 @@ mod test {
     #[test]
     fn load_lib_invalid_name() {
         let l = Library::new("name\0abcd");
-        matches!(l, Err(LoadLibErr::InvalidLibName));
+        matches!(l, Err(LoadLibError::InvalidLibName));
     }
 
     #[test]
     fn load_lib_non_existing_lib() {
         let l = Library::new("non_existing_lib_name");
-        matches!(l, Err(LoadLibErr::LibNotFound));
+        matches!(l, Err(LoadLibError::LibNotFound));
     }
 
     #[test]
@@ -80,14 +80,14 @@ mod test {
     fn get_proc_non_existent() {
         let l = Library::new(TEST_DLL_NAME).unwrap();
         let p = l.get_proc::<fn() -> ()>("NoSuchProcName");
-        matches!(p, Err(GetProcErr::ProcNotFound));
+        matches!(p, Err(GetProcError::ProcNotFound));
     }
     
     #[test]
     fn get_proc_invalid_name() {
         let l = Library::new(TEST_DLL_NAME).unwrap();
         let p = l.get_proc::<fn() -> ()>("proc\0name");
-        matches!(p, Err(GetProcErr::InvalidProcName));
+        matches!(p, Err(GetProcError::InvalidProcName));
     }
 
     #[test]
